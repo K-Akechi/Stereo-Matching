a9d07ec292afbd0a9050169c29dfc57425f77023
Chenyuhang <763467593@qq.com>
2019-04-13 18:25:39 +0800

    add new LCN method

--- train.py
+++ train.py
@@ -22,9 +22,9 @@ iterations = 2000
 def loss_fun(left_input, right_input, disp_map):
     left_reconstructed = model.image_bias_move_v2(right_input, disp_map)
     left_wlcn = wlcn(left_input, left_reconstructed)
-    optimized_loss = wb_optimization(left_input, left_wlcn)
 
-    return optimized_loss
+
+    return
 
 
 # Weighted Local Contrast Normalization
@@ -33,75 +33,34 @@ def wlcn(left, left_rc):
 
     left_slice = tf.unstack(left, axis=0)
     left_rc_slice = tf.unstack(left_rc, axis=0)
-    shape = tf.squeeze(left_slice[0]).get_shape().as_list()
     left_lcn = []
     left_rc_lcn = []
 
+    shape = left_slice[0].get_shape().as_list()
 
     for item in left_slice:
-        item = tf.squeeze(item)
-        item = tf.pad(item, [[4, 4], [4, 4]])
-        left_lcn_slice = tf.Variable(tf.zeros(shape))
-        for row in range(4, shape[0]+4):
-            for col in range(4, shape[1]+4):
-                patch = tf.slice(item, [row-4, col-4], [9, 9])
-                mean, variance = tf.nn.moments(patch, [0, 1])
-                standard_d = tf.sqrt(variance)
-                lcn_value = (item[row, col] - mean) / standard_d + 0.0001
-                one_hot = util.get_one_hot_matrix(shape[0], shape[1], [row-4, col-4])
-                left_lcn_slice = left_lcn_slice + one_hot * lcn_value
+        item = tf.reshape(item, [1, shape[0], shape[1], shape[2]])
+        left_lcn_slice = util.LocalContrastNorm(item, radius=9)
         left_lcn.append(left_lcn_slice)
+    left_lcn = tf.stack(left_lcn)
 
     for item in left_rc_slice:
-        item = tf.squeeze(item)
-        item = tf.pad(item, [[4, 4], [4, 4]])
-        left_rc_lcn_slice = tf.Variable(tf.zeros(shape))
-        for row in range(4, shape[0]+4):
-            for col in range(4, shape[1]+4):
-                patch = tf.slice(item, [row-4, col-4], [9, 9])
-                mean, variance = tf.nn.moments(patch, [0, 1])
-                standard_d = tf.sqrt(variance)
-                lcn_value = (item[row, col] - mean) / standard_d + 0.0001
-                one_hot = util.get_one_hot_matrix(shape[0], shape[1], [row - 4, col - 4])
-                left_rc_lcn_slice = left_rc_lcn_slice + one_hot * lcn_value
+        item = tf.reshape(item, [1, shape[0], shape[1], shape[2]])
+        left_rc_lcn_slice = util.LocalContrastNorm(item, radius=9)
         left_rc_lcn.append(left_rc_lcn_slice)
-
-    left_lcn = tf.stack(left_lcn)
     left_rc_lcn = tf.stack(left_rc_lcn)
+
     loss = left_lcn - left_rc_lcn
     loss_slice = tf.unstack(loss, axis=0)
-    loss_wlcn = []
+    loss_s_deviation = []
+    guassian_filter = tf.convert_to_tensor(util.get_gaussian_filter((9, 9, shape[2])))
+
     for item in loss_slice:
-        item = tf.squeeze(item)
-        item = tf.pad(item, [[4, 4], [4, 4]])
-        loss_sd_slice = tf.Variable(tf.zeros(shape))
-        for row in range(4, shape[0]+4):
-            for col in range(4, shape[1]+4):
-                patch = tf.slice(item, [row-4, col-4], [9, 9])
-                mean, variance = tf.nn.moments(patch, [0, 1])
-                standard_d = tf.sqrt(variance)
-                one_hot = util.get_one_hot_matrix(shape[0], shape[1], [row - 4, col - 4])
-                loss_sd_slice = loss_sd_slice + one_hot * standard_d
-        loss_wlcn.append(loss_sd_slice)
+        sum_square_loss = tf.nn.conv2d(tf.square(item), guassian_filter, [1, 1, 1, 1], padding='SAME')
+        s_deviation = tf.sqrt(sum_square_loss)
+        loss_s_deviation.append(s_deviation)
 
-    loss_wlcn = tf.stack(loss_wlcn)
-    return tf.abs(loss_wlcn)
-
-
-# window-based optimization
-def wb_optimization(left, wlcn):
-    left_slice = tf.unstack(left, axis=0)
-    wlcn_slice = tf.unstack(wlcn, axis=0)
-
-
-    return
-
-
-
-
-
-
-
+    return tf.stack(loss_s_deviation)
 
 
 if __name__ == "__main__":
--- util.py
+++ util.py
@@ -2,40 +2,47 @@ import tensorflow as tf
 import numpy as np
 
 
-def tensor_expand(tensor_Input,Num):
-    '''
-    张量自我复制扩展，将Num个tensor_Input串联起来，生成新的张量，
-    新的张量的shape=[tensor_Input.shape,Num]
-    :param tensor_Input:
-    :param Num:
-    :return:
-    '''
-    tensor_Input = tf.expand_dims(tensor_Input,axis=0)
-    tensor_Output = tensor_Input
-    for i in range(Num-1):
-        tensor_Output= tf.concat([tensor_Output,tensor_Input],axis=0)
-    return tensor_Output
+def get_gaussian_filter(kernel_shape):
+    x = np.zeros(kernel_shape, dtype='float32')
+
+    def gauss(x, y, sigma=2.0):
+        Z = 2 * np.pi * sigma ** 2
+        return 1. / Z * np.exp(-(x ** 2 + y ** 2) / (2. * sigma ** 2))
+
+    mid = np.floor(kernel_shape[1] / 2.)
+    for kernel_idx in range(0, kernel_shape[2]):
+        for i in range(0, kernel_shape[0]):
+            for j in range(0, kernel_shape[1]):
+                x[i, j, kernel_idx, 0] = gauss(i - mid, j - mid)
+
+    return x / np.sum(x)
 
 
-def get_one_hot_matrix(height, width, position):
-    '''
-    生成一个 one_hot矩阵，shape=【height*width】，在position处的元素为1，其余元素为0
-    :param height:
-    :param width:
-    :param position: 格式为【h_Index,w_Index】,h_Index,w_Index为int格式
-    :return:
-    '''
-    col_one_position = position[0]
-    row_one_position = position[1]
-    rows_num = height
-    cols_num = width
+def LocalContrastNorm(image, radius=9):
+    """
+    image: tf.Tensor , .shape => (1, height, width, channels)
 
-    single_row_one_hot = tf.one_hot(row_one_position, width, dtype=tf.float32)
-    single_col_one_hot = tf.one_hot(col_one_position, height, dtype=tf.float32)
+    radius: Gaussian filter size (int), odd
+    """
+    if radius % 2 == 0:
+        radius += 1
 
-    one_hot_rows = tensor_expand(single_row_one_hot, rows_num)
-    one_hot_cols = tensor_expand(single_col_one_hot, cols_num)
-    one_hot_cols = tf.transpose(one_hot_cols)
+    n, h, w, c = image.shape[0], image.shape[1], image.shape[2], image.shape[3]
+    image = tf.pad(image, [[0, 0], [radius // 2, radius // 2], [radius // 2, radius // 2], [0, 0]])
+    gaussian_filter = tf.convert_to_tensor(get_gaussian_filter((radius, radius, c, 1)))
+    filtered_out = tf.nn.conv2d(image, gaussian_filter, [1, 1, 1, 1], padding='SAME')
+    mid = int(np.floor(gaussian_filter.get_shape().as_list()[0] / 2.))
 
-    one_hot_matrix = one_hot_rows * one_hot_cols
-    return one_hot_matrix
+    ### Subtractive Normalization
+    centered_image = image - filtered_out
+    centered_image_origin_size = centered_image[:, mid:-mid, mid:-mid, :]
+    ## Variance Calc
+    sum_sqr_image = tf.nn.conv2d(tf.square(centered_image), gaussian_filter, [1, 1, 1, 1], padding='SAME')
+    s_deviation = tf.sqrt(sum_sqr_image[:, mid:-mid, mid:-mid, :])
+    # per_img_mean = tf.reduce_mean(s_deviation)
+
+    ## Divisive Normalization
+
+    divisor = s_deviation + 1e-4
+    new_image = centered_image_origin_size / divisor
+    return new_image
